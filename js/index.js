a3f212f4524b9e5cc9223771d1297218f10d816d
Danniel Rolfe <talkto@dannielrolfe.com>
2018-01-01 15:41:54 -0800

    WIP: dropdown menu

--- js/index.js
+++ js/index.js
@@ -1,4 +1,29 @@
 var app = (function() {
+
     console.log('Welcome');
+
+    function toggleNavigation() {
+        var mainNavigationButton = document.querySelector('.main-navigation-button'),
+            mainNavigationDropdown = document.querySelector('.main-navigation-links');
+
+        mainNavigationButton.addEventListener('click', function() {
+            console.log('Click');
+
+            if (mainNavigationDropdown.classList.contains('hidden')) {
+                console.log('Contains hidden');
+                mainNavigationDropdown.classList.remove('hidden');
+            } else {
+                console.log('Does not contain hidden');
+                mainNavigationDropdown.classList.add('hidden');
+            }
+        });
+    };
+
+    return {
+        toggleNavigation: toggleNavigation()
+    }
+
 })();
+
+
 
